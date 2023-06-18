16e2b1972cb86116025f64a1bad05e51dcda623b
Zabir Raihan <zabirraihan6@gmal.com>
2023-06-18 23:45:32 +0600

    Redux toolkit setup and theme file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist"; // persistStore is used to persist the store, persistReducer is used to persist the reducer, and the rest are used to configure the persistor.
+import { PersistGate } from "redux-persist/integration/react"; // stores the state in the browser's local storage
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import App from "./App";
 import "./index.css";
+import authReducer from "./state";
+
+const persistConfig = { key: "root", storage, version: 1 }; // key is the key used in the local storage
+const persistedReducer = persistReducer(persistConfig, authReducer); // persistReducer is used to persist the reducer
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE], // ignore these actions
+      },
+    }),
+}); // this store is used to persist the state.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/state/index.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// global states
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // 'functions' to update the states
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogouy: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("User friends not found");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post; // If a matching post is found, it returns the updated post from the action (action.payload.post), replacing the old post in the new updatedPosts array. If no matching post is found, it simply returns the original post, which is then included in the updatedPosts array.
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+
+export default authSlice.reducer;
