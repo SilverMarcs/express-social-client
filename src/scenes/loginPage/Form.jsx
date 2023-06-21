453600362a6fcaaa8a7744ef0871e0a763c09944
Zabir Raihan <zabirraihan6@gmal.com>
2023-06-21 16:33:44 +0600

    cors bugfix

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -90,9 +90,7 @@ const Form = () => {
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     });
 
