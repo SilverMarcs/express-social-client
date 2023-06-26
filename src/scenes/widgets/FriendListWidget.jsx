563cf168402e6e6e441b0741b4e0594317977374
Zabir Raihan <zabirraihan6@gmail.com>
2023-06-26 23:48:56 +0600

    Donw show add friend icon for self and no friends yet message in friendlist widget

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -37,17 +37,21 @@ const FriendListWidget = ({ userId }) => {
       >
         Friend List
       </Typography>
-      <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
-          <Friend
-            key={friend._id}
-            friendId={friend._id}
-            name={`${friend.firstName} ${friend.lastName}`}
-            subtitle={friend.occupation}
-            userPicturePath={friend.picturePath}
-          />
-        ))}
-      </Box>
+      {friends.length > 0 ? (
+        <Box display="flex" flexDirection="column" gap="1.5rem">
+          {friends.map((friend) => (
+            <Friend
+              key={friend._id}
+              friendId={friend._id}
+              name={`${friend.firstName} ${friend.lastName}`}
+              subtitle={friend.occupation}
+              userPicturePath={friend.picturePath}
+            />
+          ))}
+        </Box>
+      ) : (
+        <Typography color={palette.neutral.medium}>No friends yet</Typography>
+      )}
     </WidgetWrapper>
   );
 };
