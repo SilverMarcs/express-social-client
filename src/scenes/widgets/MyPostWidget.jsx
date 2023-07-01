106a7c7fecabba4aba3bfce7c44ae26619e1af42
Zabir Raihan <zabirraihan6@gmail.com>
2023-07-01 20:29:32 +0600

    Disappear image upload box after posting

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -56,6 +56,7 @@ const MyPostWidget = ({ picturePath }) => {
     const posts = await response.json();
     dispatch(setPosts({ posts }));
     setImage(null);
+    setIsImage(!isImage)
     setPost("");
   };
 
