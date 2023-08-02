25f272e5c56be0c3d61602626ac8a1e93b4a4c08
Zabir Raihan <zabirraihan6@gmail.com>
2023-08-02 14:34:26 +0800

    fix url of advert widget image

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src={`${process.env.REACT_APP_API_URL}/assets/info4.jpeg`}
+        src="https://res.cloudinary.com/dqk09fmmp/image/upload/v1690957951/info4_giivnq.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
